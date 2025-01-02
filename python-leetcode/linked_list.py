868ad087b665b8952c93bd53b59ab1619d52ec49
sarbajit100 <sarbajit.rout1000@gmail.com>
2025-01-02 19:02:00 +0530

    02-01-2025

---
+++ python-leetcode/linked_list.py
@@ -0,0 +1,62 @@
+# There is a singly-linked list head and we want to delete a node node in it.
+# You are given the node to be deleted node. You will not be given access to the first node of head.
+# All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+# Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+# The value of the given node should not exist in the linked list.
+# The number of nodes in the linked list should decrease by one.
+# All the values before node should be in the same order.
+# All the values after node should be in the same order.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        while n>0 and right:
+            right = right.next
+            n -= 1
+        while right:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return dummy.next
+		
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        pre = None
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
